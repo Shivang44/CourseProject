6313b47a5703ea24907f00690c18d165e4f44f92
Shivang Saxena <shivangs44@gmail.com>
2021-11-28 21:04:18 -0500

    Add files via upload

---
+++ chrome_extension/content-script.js
@@ -0,0 +1,30 @@
+
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log("In content-script:", request);
+
+        if (request.annotations) {
+            request.annotations.forEach((annotation) => {
+                const para = document.createElement("p");
+                const paraNode = document.createTextNode(annotation.content);
+                para.style = "background-color:#FFFFCC;color:black;padding:25px;";
+                para.appendChild(paraNode);
+                para.dataset.lineNumber = annotation.line_number;
+                para.addEventListener('click', e => {
+                    // Unrender item
+                    e.target.style = "display: none";
+
+                    // Delete from storage
+                    sendResponse({annotation_id_to_delete: e.target.dataset.lineNumber});
+                });
+                
+                const node = document.getElementById("L" + annotation.line_number);
+                if (node) {
+                    node.appendChild(para);
+                }
+            });
+        }
+    }
+  );
+  
